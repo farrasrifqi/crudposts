e11e1b5b429de33e4827ab3acd9d10bd8df3edec
farrasrifqi <farrasrifqi25@gmail.com>
2024-07-22 09:52:32 +0700

    membuat crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', PostController::class .'@index')->name('posts.index');
+Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
+Route::post('/posts', PostController::class .'@store')->name('posts.store');
+Route::get('/posts/{post}', PostController::class .'@show')->name('posts.show');
+Route::get('/posts/{post}/edit', PostController::class .'@edit')->name('posts.edit');
+Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
+Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
