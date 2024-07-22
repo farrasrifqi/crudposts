a9c086397f08eec545a8eabd30bdc6be74d8668b
farrasrifqi <farrasrifqi25@gmail.com>
2024-07-22 10:51:08 +0700

    menambah file kedalam resource

--- app/Console/Commands/MakeViewCommand.php
+++ app/Console/Commands/MakeViewCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class MakeViewCommand extends Command
 {
@@ -25,6 +26,30 @@ class MakeViewCommand extends Command
      */
     public function handle()
     {
-        
+        $view = $this->argument('view');
+        $path = $this->viewPath($view);
+        $this->createDir($path);
+        if (File::exists($path)) {
+            $this->error("File {$path} already exists!");
+            return;
+        }
+        File::put($path, $path);
+        $this->info("File {$path} created.");
+    }
+
+    public function viewPath($view)
+    {
+        $view = str_replace('.', '/', $view) . '.blade.php';
+        $path = "resources/views/{$view}";
+        return $path;
+    }
+
+    public function createDir($path)
+    {
+        $dir = dirname($path);
+        if (!file_exists($dir)) 
+        {
+            mkdir($dir, 0777, true);
+        }
     }
 }
