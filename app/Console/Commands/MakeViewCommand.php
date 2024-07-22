d84dc9366ea076156088e8bdc2ff94d6053cf03d
farrasrifqi <farrasrifqi25@gmail.com>
2024-07-22 10:13:00 +0700

    menambahknan laravel bled

---
+++ app/Console/Commands/MakeViewCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeViewCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:view {view}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new blade tempalate.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        
+    }
+}
